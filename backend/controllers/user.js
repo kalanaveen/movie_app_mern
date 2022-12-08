2035b4019c9af9848faa49954fec4ffdb7f75af9
naveen kala <kalanaveen55@gmail.com>
2022-12-08 15:56:59 +0530

    hashing password

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,8 @@
-exports.create = (req, res) => {
-    console.log(req.body);
-    res.send('<h1>will create later</h1>')
-}
+const User = require("../models/user");
+
+exports.create = async(req, res) => {
+    const { name, email, password } = req.body;
+    const newUser = new User({ name, email, password });
+    await newUser.save();
+    res.json({ user: newUser });
+};
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,21 +1,28 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = mongoose.Schema({
     name: {
         type: String,
         trim: true,
-        required:true
+        required: true
     },
     email: {
         type: String,
         required: true,
         trim: true,
-        unique:true
+        unique: true
     },
     password: {
         type: String,
-        required:true
+        required: true
     }
-})
+});
 
-mondule.exports = mongoose.model('User', userSchema);
+userSchema.pre("save", async function(next){
+    if(this.isModified("password")){
+    this.password = await bcrypt.hash(this.password, 10);
+}
+next()
+})
+module.exports = mongoose.model('User', userSchema);

a53931bfbc8cf4cc52bcdde680ad4cc408d00b55
naveen kala <kalanaveen55@gmail.com>
2022-12-16 13:11:14 +0530

    error handler added

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,19 @@
+require('express-async-errors');
 const express = require('express');
 const userRouter = require('./routes/user');
 const morgan = require('morgan');
+const { errorHandler } = require('./middleware/error');
 require('dotenv').config();
-require('./db')
+require('./db');
+
 const app = express();
 
 app.use(express.json());
 app.use(morgan('dev'));
-app.use('/api/user',userRouter);
+app.use('/api/user', userRouter);
+
+app.use(errorHandler);
+
 app.listen(8000, () => {
     console.log("the port is listening on port 8000");
 })
